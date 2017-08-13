1a3acf0c70700dd8ff04f38849f7dcfeb07f347c
yonidavidson <yoni@sears.co.il>
2017-08-14 01:29:57 +0300

    use regex switch

--- src/handler/__tests__/pre.spec.js
+++ src/handler/__tests__/pre.spec.js
@@ -5,6 +5,9 @@ describe('preprocess tokens', () => {
 		{ input: ' j  =  987', expected: 'j = 987'},
         { input: 'j=987', expected: 'j = 987'},
 		{ input: 'a  +=   120', expected: 'a #1 120'},
+        { input: 'a+=120', expected: 'a #1 120'},
+        { input: '2 + ++j', expected: '2 + #2j'},
+        { input: 'a+ ++h', expected: 'a + #2h'}
 	].forEach(testCase => {
 		it('should tokenize the string', () => {
 			const result = uut(testCase.input)
--- src/handler/pre.js
+++ src/handler/pre.js
@@ -1,6 +1,7 @@
 const pre = (input) => {
     let response = input.replace(/\+\=/g, ' #1 ');
-    response = response.replace(/\+{1}/g, ' + ');
+    response = response.replace(/([\d,a-z,A-Z])\+{1}/g, '$1 + ');
+    response = response.replace(/\+{2}/g, ' #2');
     response = response.replace(/\={1}/g, ' = ');
     response = response.replace(/\s\s+/g, ' ');
     return response.trim()
