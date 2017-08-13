ed9ea5ffd21f6b031a7f25ad0441d928d6b2aa4a
yonidavidson <yoni@sears.co.il>
2017-08-14 02:26:11 +0300

    semi works

--- src/handler/__tests__/pre.spec.js
+++ src/handler/__tests__/pre.spec.js
@@ -4,10 +4,12 @@ describe('preprocess tokens', () => {
 	[
 		{ input: ' j  =  987', expected: 'j = 987'},
         { input: 'j=987', expected: 'j = 987'},
-		{ input: 'a  +=   120', expected: 'a #1 120'},
-        { input: 'a+=120', expected: 'a #1 120'},
+		{ input: 'a  +=   120', expected: 'a @1 120'},
+        { input: 'a+=120', expected: 'a @1 120'},
         { input: '2 + ++j', expected: '2 + #2j'},
-        { input: 'a+ ++h', expected: 'a + #2h'}
+        { input: 'a+ ++h', expected: 'a + #2h'},
+        { input: '2 + j++', expected: '2 + #3j'},
+        { input: 'a+ h++', expected: 'a + #3h'}
 	].forEach(testCase => {
 		it('should tokenize the string', () => {
 			const result = uut(testCase.input)
--- src/handler/pre.js
+++ src/handler/pre.js
@@ -1,8 +1,10 @@
 const pre = (input) => {
-    let response = input.replace(/\+\=/g, ' #1 ');
-    response = response.replace(/([\d,a-z,A-Z])\+{1}/g, '$1 + ');
-    response = response.replace(/\+{2}/g, ' #2');
-    response = response.replace(/\={1}/g, ' = ');
+    let response = input;
+    response = response.replace(/\s\++([a-zA-Z])/g, ' #2$1');
+    response = response.replace(/([a-z,A-Z])\+{2}/g, ' #3$1');
+    response = response.replace(/\+\=/g, ' @1 ');
+    response = response.replace(/([\s,\d,a-z,A-Z])\+[\s,\d,a-z,A-Z]/g, '$1 + ');
+    response = response.replace(/([\d\sa-zA-Z])(\=)/g, '$1 $2 ');
     response = response.replace(/\s\s+/g, ' ');
     return response.trim()
 };
