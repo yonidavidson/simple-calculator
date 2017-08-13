4c9aaaa12eef17e2229200cc1063b2ddc6a5fdfe
yonidavidson <yoni@sears.co.il>
2017-08-14 02:39:36 +0300

    samples from exce work

--- src/handler/__tests__/pre.spec.js
+++ src/handler/__tests__/pre.spec.js
@@ -10,10 +10,12 @@ describe('preprocess tokens', () => {
         { input: 'a+ ++h', expected: 'a + #2h'},
         { input: '2 + j++', expected: '2 + #3j'},
         { input: 'a+ h++', expected: 'a + #3h'},
-        // { input: 'i = 0', expected: 'i = 0'},
-        // { input: 'j = i++', expected: 'j = #3j'},
-        // { input: 'x = i++ + 5', expected: 'x = #3i + 5'},
-
+        { input: 'i = 0', expected: 'i = 0'},
+        { input: 'j = i++', expected: 'j = #3i'},
+        { input: 'x = i++ + 5', expected: 'x = #3i + 5'},
+        { input: 'x = i++ + 5', expected: 'x = #3i + 5'},
+        { input: 'y = 5+3 * 10', expected: 'y = 5 + 3 * 10'},
+        { input: 'i+=y', expected: 'i = i + y'},
 	].forEach(testCase => {
 		it('should tokenize the string', () => {
 			const result = uut(testCase.input)
