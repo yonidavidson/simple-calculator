09ebb3a197d990a13a3e8dc48a120993cfd394fe
yonidavidson <yoni@sears.co.il>
2017-08-14 02:30:15 +0300

    add tests for test

--- src/handler/__tests__/pre.spec.js
+++ src/handler/__tests__/pre.spec.js
@@ -9,7 +9,11 @@ describe('preprocess tokens', () => {
         { input: '2 + ++j', expected: '2 + #2j'},
         { input: 'a+ ++h', expected: 'a + #2h'},
         { input: '2 + j++', expected: '2 + #3j'},
-        { input: 'a+ h++', expected: 'a + #3h'}
+        { input: 'a+ h++', expected: 'a + #3h'},
+        // { input: 'i = 0', expected: 'i = 0'},
+        // { input: 'j = i++', expected: 'j = #3j'},
+        // { input: 'x = i++ + 5', expected: 'x = #3i + 5'},
+
 	].forEach(testCase => {
 		it('should tokenize the string', () => {
 			const result = uut(testCase.input)
