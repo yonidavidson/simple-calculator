af1d42dd904d809fad5ec90cae7ed9423c4c8dcd
yonidavidson <yoni@sears.co.il>
2017-08-14 00:21:40 +0300

    Start tokenizing

--- src/handler/__tests__/pre.spec.js
+++ src/handler/__tests__/pre.spec.js
@@ -2,8 +2,8 @@ import uut from '../pre';
 
 describe('preprocess tokens', () => {
 	[
-		{ input: 'j =  987', expected: 'j = 987'},
-		{ input: 'a += 120', expected: 'a = a + 120'},
+		{ input: ' j  =  987', expected: 'j = 987'},
+		{ input: 'a  +=   120', expected: 'a #1 120'},
 	].forEach(testCase => {
 		it('should tokenize the string', () => {
 			const result = uut(testCase.input)
--- src/handler/pre.js
+++ src/handler/pre.js
@@ -1,5 +1,7 @@
 const pre = (input) => {
-    return input
+    let response = input.replace(/\+\=/g, ' #1 ');
+    response = response.replace(/\s\s+/g, ' ');
+    return response.trim()
 };
 
 export default pre;
