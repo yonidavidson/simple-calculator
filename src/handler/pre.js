3f7da127e541d939772c241882b0b511702683c2
yonidavidson <yonidavidson@gmail.com>
2017-08-14 23:36:03 +0300

    rename vars

--- src/handler/pre.js
+++ src/handler/pre.js
@@ -1,7 +1,7 @@
 const pre = input => {
 	let response = input;
-	response = response.replace(/\s\++([a-zA-Z])/g, ' #2$1');
-	response = response.replace(/([a-z,A-Z])\+{2}/g, ' #3$1');
+	response = response.replace(/\s\++([a-zA-Z])/g, ' #{++L}$1');
+	response = response.replace(/([a-z,A-Z])\+{2}/g, ' #{++R}$1');
 	response = response.replace(/([a-z,A-Z])\+\=/g, '$1 = $1 + ');
 	response = response.replace(
 		/([\s,\d,a-z,A-Z])([\+,\*])([\s,\d,a-z,A-Z])/g,
