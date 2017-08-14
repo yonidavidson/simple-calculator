968787e528016cb52290f2e75bfba36d8ec917e5
yonidavidson <yonidavidson@gmail.com>
2017-08-14 23:44:12 +0300

    fix small bug

--- src/handler/__tests__/pre.spec.js
+++ src/handler/__tests__/pre.spec.js
@@ -2,23 +2,23 @@ import uut from '../pre';
 
 describe('preprocess tokens', () => {
 	[
-		{ input: ' j  =  987', expected: 'j = 987'},
-        { input: 'j=987', expected: 'j = 987'},
-		{ input: 'a+=c', expected: 'a = a + c'},
-        { input: 'a+=120', expected: 'a = a + 120'},
-        { input: '2 + ++j', expected: '2 + #{++L}j'},
-        { input: 'a+ ++h', expected: 'a + #{++L}h'},
-        { input: '2 + j++', expected: '2 + #{++R}j'},
-        { input: 'a+ h++', expected: 'a + #{++R}h'},
-        { input: 'i = 0', expected: 'i = 0'},
-        { input: 'j = i++', expected: 'j = #{++R}i'},
-        { input: 'x = i++ + 5', expected: 'x = #{++R}i + 5'},
-        { input: 'x = i++ + 5', expected: 'x = #{++R}i + 5'},
-        { input: 'y = 5+3 * 10', expected: 'y = 5 + 3 * 10'},
-        { input: 'i+=y', expected: 'i = i + y'},
+		{ input: ' j  =  987', expected: 'j = 987' },
+		{ input: 'j=987', expected: 'j = 987' },
+		{ input: 'a+=c', expected: 'a = a + c' },
+		{ input: 'a+=120', expected: 'a = a + 120' },
+		{ input: '2 + ++j', expected: '2 + #{++L}j' },
+		{ input: 'a+ ++h', expected: 'a + #{++L}h' },
+		{ input: '2 + j++', expected: '2 + #{++R}j' },
+		{ input: 'a+ h++', expected: 'a + #{++R}h' },
+		{ input: 'i = 0', expected: 'i = 0' },
+		{ input: 'j = i++', expected: 'j = #{++R}i' },
+		{ input: 'x = i++ + 5', expected: 'x = #{++R}i + 5' },
+		{ input: 'x = i++ + 514', expected: 'x = #{++R}i + 514' },
+		{ input: 'y = 512+ 312 * 10', expected: 'y = 512 + 312 * 10' },
+		{ input: 'i+=y', expected: 'i = i + y' },
 	].forEach(testCase => {
 		it('should tokenize the string', () => {
-			const result = uut(testCase.input)
+			const result = uut(testCase.input);
 
 			expect(result).toBe(testCase.expected);
 		});
--- src/handler/pre.js
+++ src/handler/pre.js
@@ -1,14 +1,11 @@
 const pre = input => {
 	let response = input;
-	response = response.replace(/\s\++([a-zA-Z])/g, ' #{++L}$1');
-	response = response.replace(/([a-z,A-Z])\+{2}/g, ' #{++R}$1');
-	response = response.replace(/([a-z,A-Z])\+\=/g, '$1 = $1 + ');
-	response = response.replace(
-		/([\s,\d,a-z,A-Z])([\+,\*])([\s,\d,a-z,A-Z])/g,
-		'$1 $2 $3 ',
-	);
-	response = response.replace(/([\d\sa-zA-Z])(\=)/g, '$1 $2 ');
-	response = response.replace(/\s\s+/g, ' ');
+	response = response.replace(/\s\+{2}([a-z])/gi, ' #{++L}$1');
+	response = response.replace(/([a-z])\+{2}/gi, ' #{++R}$1');
+	response = response.replace(/([a-z])\+\=/gi, '$1 = $1 + ');
+	response = response.replace(/([\s\da-z])([\+\*])([^\+\*])/gi, '$1 $2 $3 ');
+	response = response.replace(/([\d\sa-zA-Z])(\=)/gi, '$1 $2 ');
+	response = response.replace(/\s\s+/gi, ' ');
 	return response.trim();
 };
 
