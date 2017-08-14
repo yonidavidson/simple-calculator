9642ea0e0617b338df36912c467ad89bc14a616f
yonidavidson <yonidavidson@gmail.com>
2017-08-14 20:33:17 +0300

    testing process

--- src/handler/__tests__/memory.spec.js
+++ src/handler/__tests__/memory.spec.js
@@ -2,14 +2,19 @@ import uut from '../memory';
 
 describe('preprocess tokens', () => {
 	[
-		{ mem:{c:2}, input: 'a + c', expected: {text:'0 + 2' ,mem:{c:2}}}
-        // { mem:{j:3}, input: '2 + #2j', expected: {text:'2 + 4' ,mem{j:4}},
-        // { mem:{j:3}, input: '2 + #3j', expected: {text:'2 + 4' ,mem{j:3}},
+		{ mem: {}, input: 'a + c', expected: { text: '0 + 0', mem: {} } },
+		{
+			mem: { c: 2 },
+			input: 'a + c',
+			expected: { text: '0 + 2', mem: { c: 2 } },
+		},
+		// { mem:{j:3}, input: '2 + #2j', expected: {text:'2 + 4' ,mem{j:4}},
+		// { mem:{j:3}, input: '2 + #3j', expected: {text:'2 + 4' ,mem{j:3}},
 	].forEach(testCase => {
 		it('should update memory and return only numbers and primitives', () => {
-			const result = uut(testCase.input, testCase.mem)
+			const result = uut(testCase.input, testCase.mem);
 
-			expect(result).toBe(testCase.expected);
+			expect(result).toEqual(testCase.expected);
 		});
 	});
 });
--- src/handler/memory.js
+++ src/handler/memory.js
@@ -1,18 +1,26 @@
 const memory = (input, mem) => {
-    let text = input;
-    text = text.split(' ');
-    text = text.map(x => {
-    switch (typeof x) {
-    case string:
-        return "str";
-    case number:
-        return "num";
-    default:
-        return "other";
-    }
-    })
-    text = text.trim();
-    return {text, mem}
+	let text = input;
+	text = text.split(' ');
+	text = text.map(x => {
+		if (isNumeric(x) || isOperator(x)) {
+			return x;
+		}
+		return handleVar(x, mem);
+	});
+	text = text.join(' ');
+	return { text, mem };
 };
 
 export default memory;
+
+function isOperator(str) {
+	return str.length === 1 && str.match(/[\+\-\*\/]/);
+}
+
+function isNumeric(num) {
+	return !isNaN(num);
+}
+
+function handleVar(x, mem) {
+	return mem.hasOwnProperty(x) ? mem[x] : 0;
+}
